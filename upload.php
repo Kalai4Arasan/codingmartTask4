920184e2ebe10dd9261fb2fcd625e58023a96028
Kalai4Arasan <47599995+Kalai4Arasan@users.noreply.github.com>
2020-06-12 08:58:01 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,6 +2,11 @@
 $len=$_POST['len'];
 for($i=0;$i<$len;$i++){
     print_r($_FILES['file'.$i]['tmp_name']);
+    $dir='uploadedImages';
+    if( is_dir($dir) === false )
+    {
+        mkdir($dir);
+    }
     move_uploaded_file($_FILES['file'.$i]['tmp_name'],'./uploadedImages/'.$_FILES['file'.$i]['name']);
 }
 ?>
